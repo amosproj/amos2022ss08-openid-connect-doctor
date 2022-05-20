70c9f63ff9dc41d136fcddb647d8e454d500162c
Michael Kupfer <michael.kupfer@fau.de>
2022-05-20 22:35:51 +0200

    Check for provider existance

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Render, Query } from '@nestjs/common';
+import { Issuer } from 'openid-client';
 
 @Controller()
 export class AppController {
@@ -8,9 +9,23 @@ export class AppController {
     return;
   }
 
+  async get_issuer(issuer_s) {
+    const issuer = await Issuer.discover(issuer_s);
+    return issuer;
+  }
+
   @Get('issuer')
   @Render('index')
-  discover_issuer(@Query('issuer') issuer: string) {
-    return { result: issuer };
+  async discover_issuer(@Query('issuer') issuer_s: string) {
+    return {
+      result: await this.get_issuer(issuer_s)
+        .then((issuer) => {
+          console.log(issuer);
+          return "Provider exists on endpoint: " + issuer.authorization_endpoint;
+        })
+        .catch((err) => {
+          return err;
+        }),
+    };
   }
 }

411ee2ddc108e8cf0f28f12ebe767bc328b6bd93
raghunandanarava <raghunandan.arava@fau.de>
2022-06-01 08:24:11 +0200

    Implementation of #6
    Signed-off-by: raghunandanarava <raghunandan.arava@fau.de>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,22 @@
-import { Controller, Get, Render, Redirect, Query } from '@nestjs/common';
-import { Issuer } from 'openid-client';
+import {
+  Controller,
+  Get,
+  Render,
+  Query,
+  HttpException,
+  HttpStatus,
+  Res,
+  Post,
+  Body,
+} from '@nestjs/common';
+import { Issuer, GrantBody } from 'openid-client';
+import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get()
   @Render('index')
   root() {
@@ -17,29 +31,37 @@ export class AppController {
   @Get('issuer')
   @Render('index')
   async discover_issuer(
-      @Query('issuer_url') issuer_url_s: string,
-      @Query('authorization_endpoint') authorization_endpoint_s: string,
-      @Query('claim_types_supported') claim_types_supported_s: string,
-      @Query('claims_parameter_supported') claims_parameter_supported_s: string,
-      @Query('claims_supported') claims_supported_s: string,
-      @Query('code_challenge_methods_supported') code_challenge_methods_supported_s: string,
-      @Query('device_authorization_endpoint') device_authorization_endpoint_s: string,
-      @Query('grant_types_supported') grant_types_supported_s: string,
-      @Query('id_token_signing_alg_values_supported') id_token_signing_alg_values_supported_s: string,
-      @Query('issuer') issuer_s: string,
-      @Query('jwks_uri') jwks_uri_s: string,
-      @Query('request_parameter_supported') request_parameter_supported_s: string,
-      @Query('request_uri_parameter_supported') request_uri_parameter_supported_s: string,
-      @Query('require_request_uri_registration') require_request_uri_registration_s: string,
-      @Query('response_modes_supported') response_modes_supported_s: string,
-      @Query('response_types_supported') response_types_supported_s: string,
-      @Query('revocation_endpoint') revocation_endpoint_s: string,
-      @Query('revocation_endpoint_auth_methods_supported') revocation_endpoint_auth_methods_supported_s: string,
-      @Query('scopes_supported') scopes_supported_s: string,
-      @Query('subject_types_supported') subject_types_supported_s: string,
-      @Query('token_endpoint') token_endpoint_s: string,
-      @Query('token_endpoint_auth_methods_supported') token_endpoint_auth_methods_supported_s: string,
-      @Query('userinfo_endpoint') userinfo_endpoint_s: string) {
+    @Query('issuer_url') issuer_url_s: string,
+    @Query('authorization_endpoint') authorization_endpoint_s: string,
+    @Query('claim_types_supported') claim_types_supported_s: string,
+    @Query('claims_parameter_supported') claims_parameter_supported_s: string,
+    @Query('claims_supported') claims_supported_s: string,
+    @Query('code_challenge_methods_supported')
+    code_challenge_methods_supported_s: string,
+    @Query('device_authorization_endpoint')
+    device_authorization_endpoint_s: string,
+    @Query('grant_types_supported') grant_types_supported_s: string,
+    @Query('id_token_signing_alg_values_supported')
+    id_token_signing_alg_values_supported_s: string,
+    @Query('issuer') issuer_s: string,
+    @Query('jwks_uri') jwks_uri_s: string,
+    @Query('request_parameter_supported') request_parameter_supported_s: string,
+    @Query('request_uri_parameter_supported')
+    request_uri_parameter_supported_s: string,
+    @Query('require_request_uri_registration')
+    require_request_uri_registration_s: string,
+    @Query('response_modes_supported') response_modes_supported_s: string,
+    @Query('response_types_supported') response_types_supported_s: string,
+    @Query('revocation_endpoint') revocation_endpoint_s: string,
+    @Query('revocation_endpoint_auth_methods_supported')
+    revocation_endpoint_auth_methods_supported_s: string,
+    @Query('scopes_supported') scopes_supported_s: string,
+    @Query('subject_types_supported') subject_types_supported_s: string,
+    @Query('token_endpoint') token_endpoint_s: string,
+    @Query('token_endpoint_auth_methods_supported')
+    token_endpoint_auth_methods_supported_s: string,
+    @Query('userinfo_endpoint') userinfo_endpoint_s: string,
+  ) {
     const checkboxes = {
       authorization_endpoint: authorization_endpoint_s,
       claim_types_supported: claim_types_supported_s,
@@ -48,7 +70,8 @@ export class AppController {
       code_challenge_methods_supported: code_challenge_methods_supported_s,
       device_authorization_endpoint: device_authorization_endpoint_s,
       grant_types_supported: grant_types_supported_s,
-      id_token_signing_alg_values_supported: id_token_signing_alg_values_supported_s,
+      id_token_signing_alg_values_supported:
+        id_token_signing_alg_values_supported_s,
       issuer: issuer_s,
       jwks_uri: jwks_uri_s,
       request_parameter_supported: request_parameter_supported_s,
@@ -57,11 +80,13 @@ export class AppController {
       response_modes_supported: response_modes_supported_s,
       response_types_supported: response_types_supported_s,
       revocation_endpoint: revocation_endpoint_s,
-      revocation_endpoint_auth_methods_supported: revocation_endpoint_auth_methods_supported_s,
+      revocation_endpoint_auth_methods_supported:
+        revocation_endpoint_auth_methods_supported_s,
       scopes_supported: scopes_supported_s,
       subject_types_supported: subject_types_supported_s,
       token_endpoint: token_endpoint_s,
-      token_endpoint_auth_methods_supported: token_endpoint_auth_methods_supported_s,
+      token_endpoint_auth_methods_supported:
+        token_endpoint_auth_methods_supported_s,
       userinfo_endpoint: userinfo_endpoint_s,
     };
     let keys = [];
@@ -98,5 +123,45 @@ export class AppController {
           };
         }),
     };
+  }
+
+  @Get('token')
+  async requestToken(
+    @Query('issuer') issuer_s: string,
+    @Res() res: Response,
+  ): Promise<any> {
+    const issuer = await this.get_issuer(issuer_s).catch(() => {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'invalid issuer',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    });
+    const result = await this.appService.requestToken(issuer);
+    res.json(result.data).send();
+  }
+
+  @Post('/token')
+  async requestTokenWithClientInformation(
+    @Query('issuer') issuer_s: string,
+    @Body() grantBody: GrantBody,
+    @Res() res: Response,
+  ): Promise<any> {
+    const issuer = await this.get_issuer(issuer_s).catch(() => {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'invalid issuer',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    });
+    const result = await this.appService.getToken(
+      String(issuer.token_endpoint),
+      grantBody,
+    );
+    res.json(result.data).send();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { GrantBody, Issuer } from 'openid-client';
+import axios from 'axios';
+import qs from 'qs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getToken(token_endpoint: string, grantBody: GrantBody): Promise<any> {
+    return await axios
+      .post(token_endpoint, qs.stringify(grantBody), {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .catch(() => {
+        throw new HttpException(
+          {
+            status: HttpStatus.UNAUTHORIZED,
+            error: 'Access denied',
+          },
+          HttpStatus.UNAUTHORIZED,
+        );
+      });
+  }
+
+  async requestToken(issuer: Issuer): Promise<any> {
+    const grantBody: GrantBody = {
+      grant_type: process.env.CLIENT_CREDENTIALS_STRING,
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      audience: process.env.AUDIENCE,
+    };
+
+    return await this.getToken(String(issuer.token_endpoint), grantBody);
   }
 }

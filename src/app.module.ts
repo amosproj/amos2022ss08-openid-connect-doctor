16bdc64ad3dc659d008626b05e0c05126cd3018c
raghunandanarava <raghunandan.arava@fau.de>
2022-06-01 08:24:11 +0200

    Implementation of #6
    Signed-off-by: raghunandanarava <raghunandan.arava@fau.de>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [ConfigModule.forRoot(), UserModule],
   providers: [AppService],
   controllers: [AppController],
 })

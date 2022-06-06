35359dc921c3b314d35d964b4e15de51888d06a7
RumiAust <rumi019aust@gmail.com>
2022-06-06 11:51:52 +0200

    login page added #44

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {Body, Controller, Get, Post, Render} from '@nestjs/common';
 import { UserService } from './user.service';
 import { LoginUserDto } from './Dto/LoginUser.dto';
 
@@ -10,4 +10,11 @@ export class UserController {
   login(@Body() loginUserDto: LoginUserDto) {
     return this.userService.login(loginUserDto);
   }
+
+  @Get('login')
+  @Render('login')
+  root() {
+    return;
+  }
+
 }

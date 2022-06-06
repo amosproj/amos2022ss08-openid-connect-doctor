27844a3bc449012f75a1fb8c3b20a3f6b277533b
RumiAust <rumi019aust@gmail.com>
2022-06-06 11:20:11 +0200

    discover service test case implemented #44

--- src/discovery/discovery.service.spec.ts
+++ src/discovery/discovery.service.spec.ts
@@ -15,4 +15,18 @@ describe('DiscoveryService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('get_issuer', () => {
+    it('should fail if no issuer is provided', async () => {
+      await expect(service.get_issuer(undefined)).rejects.toThrow(
+          'There was no issuer string passed to get the issuer',
+      );
+    });
+
+    it('should fail is an empty issuer is provided', async () => {
+      await expect(service.get_issuer('')).rejects.toThrow(
+          'There was no issuer string passed to get the issuer',
+      );
+    });
+  });
 });
--- src/discovery/discovery.service.ts
+++ src/discovery/discovery.service.ts
@@ -1,10 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
 import {Issuer} from "openid-client";
 
 @Injectable()
 export class DiscoveryService {
 
     async get_issuer(issuer_s) {
+        if(issuer_s === undefined || issuer_s === ''){
+            throw new HttpException(
+                'There was no issuer string passed to get the issuer',
+                HttpStatus.BAD_REQUEST,
+            );
+        }
         const issuer = await Issuer.discover(issuer_s);
         return issuer;
     }

a04975c4b8604e328a2685a485f5123b4cb274e5
RumiAust <rumi019aust@gmail.com>
2022-06-08 01:50:15 +0200

    backend logic added to control logger toggle from frontend  #39

--- src/protocol/protocol.controller.ts
+++ src/protocol/protocol.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Query, Render} from '@nestjs/common';
+import {ProtocolService} from "./protocol.service";
 
 @Controller('protocol')
 export class ProtocolController {
+    constructor(private readonly protocolService: ProtocolService) {}
+    @Get('toggleLogger')
+    async discover_issuer(
+        @Query('toggleLogger') toggleLogger: number){
+         this.protocolService.toggleWriteStatus(toggleLogger);
+    }
 
 }
--- src/protocol/protocol.service.ts
+++ src/protocol/protocol.service.ts
@@ -3,20 +3,28 @@ import {Injectable, Logger} from '@nestjs/common';
 @Injectable()
 export class ProtocolService {
     logger: Logger;
+    toggle:number=1;
     constructor() {
         this.logger = new Logger(ProtocolService.name);
+    }
+    toggleWriteStatus(flag:number){
+        this.toggle=flag;
     }
 
     writeLoggerToFile(logMessage:string): void {
         const fs = require('fs');
         let dateTime = new Date();
         const dirPath="../amos2022ss08-openid-connect-doctor/src/protocol/";
-        if(fs.existsSync(dirPath)){
-            fs.writeFileSync(dirPath+"/logger.txt",dateTime+" :: "+logMessage+"\n", {flag:"a"});
-           return  this.logger.log("write Successful")
+        if(this.toggle==1) {
+            if (fs.existsSync(dirPath)) {
+                fs.writeFileSync(dirPath + "/logger.txt", dateTime + " :: " + logMessage + "\n", {flag: "a"});
+                return this.logger.log("write Successful")
 
+            } else {
+                return this.logger.error(`${dirPath} not found`)
+            }
         }else{
-          return  this.logger.error(`${dirPath} not found`)
+            return this.logger.warn("No write permission in the logger file.Enable Checkbox to write logs in the file");
         }
 
     }

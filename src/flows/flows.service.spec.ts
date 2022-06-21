2da213457fd4fba1201687481afc339a98811433
raghunandanarava <raghunandan.arava@fau.de>
2022-06-21 23:42:42 +0200

    Updated test cases for FlowsService #59
    Signed-off-by: raghunandanarava <raghunandan.arava@fau.de>

--- src/flows/flows.service.spec.ts
+++ src/flows/flows.service.spec.ts
@@ -18,4 +18,42 @@ describe('FlowsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('clientCredentials', () => {
+    it('should fail if no issuer is provided', async () => {
+      await expect(
+        service.clientCredentials(undefined, 'client_id', 'client_secret'),
+      ).rejects.toThrow('There was no issuer to validate the token against!');
+    });
+
+    it('should fail if issuer is empty', async () => {
+      await expect(
+        service.clientCredentials('', 'client_id', 'client_secret'),
+      ).rejects.toThrow('There was no issuer to validate the token against!');
+    });
+
+    it('should fail if no client id is provided', async () => {
+      await expect(
+        service.clientCredentials('issuer_s', undefined, 'client_secret'),
+      ).rejects.toThrow('There was no client id provided');
+    });
+
+    it('should fail if client id is empty', async () => {
+      await expect(
+        service.clientCredentials('issuer_s', '', 'client_secret'),
+      ).rejects.toThrow('There was no client id provided');
+    });
+
+    it('should fail if no client secret is provided', async () => {
+      await expect(
+        service.clientCredentials('issuer_s', 'client_id', undefined),
+      ).rejects.toThrow('There was no client secret provided');
+    });
+
+    it('should fail if client secret is empty', async () => {
+      await expect(
+        service.clientCredentials('issuer_s', 'client_id', ''),
+      ).rejects.toThrow('There was no client secret provided');
+    });
+  });
 });

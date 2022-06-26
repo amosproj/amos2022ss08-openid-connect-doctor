24146493b6d2bfb8eeeff9cd0e8228db95922fc2
raghunandanarava <raghunandan.arava@fau.de>
2022-06-26 13:08:08 +0200

    Added Flows #59

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,23 @@
 //SDPX-License-Identifier: MIT
 //SDPX-FileCopyrightText: 2022 Philip Rebbe <rebbe.philip@fau.de>
 
-import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { TokenModule } from './token/token.module';
 import { DiscoveryModule } from './discovery/discovery.module';
 import { FlowsModule } from './flows/flows.module';
 import { ProtocolModule } from './protocol/protocol.module';
-import {ProtocolService} from "./protocol/protocol.service";
-import {ProtocolController} from "./protocol/protocol.controller";
-import {LoggerMiddleware} from "./utils/logger.middleware";
+import { ProtocolService } from './protocol/protocol.service';
+import { ProtocolController } from './protocol/protocol.controller';
+import { LoggerMiddleware } from './utils/logger.middleware';
 import { SettingsModule } from './settings/settings.module';
 import { HelperModule } from './helper/helper.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    UserModule,
     DiscoveryModule,
     TokenModule,
     ProtocolModule,
@@ -30,7 +28,7 @@ import { HelperModule } from './helper/helper.module';
   providers: [AppService, ProtocolService],
   controllers: [AppController, ProtocolController],
 })
-export class AppModule  implements NestModule{
+export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('*');
   }

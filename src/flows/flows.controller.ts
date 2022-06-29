728de8988a53453a7e6742d021d3a51ff3c44f6a
Michael Kupfer <mkupfer99@gmail.com>
2022-06-29 18:26:45 +0200

    First revision password grant

--- src/flows/flows.controller.ts
+++ src/flows/flows.controller.ts
@@ -4,6 +4,7 @@
 
 import { Controller, Get, Post, Body, Render } from '@nestjs/common';
 import { ClientCredentialFlowInputDto } from './Dto/clientCredentialFlowInput.dto';
+import { PasswordGrantFlowInputDto } from './Dto/passwordGrantFlowInput.dto';
 import { FlowsService } from './flows.service';
 
 @Controller('flows')
@@ -49,6 +50,47 @@ export class FlowsController {
         };
       });
 
+    return result;
+  }
+
+  @Get('pg')
+  @Render('password_grant')
+  async getPg() {
+    return;
+  }
+
+  @Post('pg')
+  @Render('cc-result')
+  async postPg(@Body() passwordGrantFlowInputDto: PasswordGrantFlowInputDto) {
+    console.log(passwordGrantFlowInputDto);
+    const result = await this.flowsService
+      .passwordGrant(
+        passwordGrantFlowInputDto.issuerUrl,
+        passwordGrantFlowInputDto.clientId,
+        passwordGrantFlowInputDto.clientSecret,
+        passwordGrantFlowInputDto.username,
+        passwordGrantFlowInputDto.password,
+      )
+      .then(([discoveryResult, decodingResult]) => {
+        return {
+          showResults: decodingResult.success,
+          message: decodingResult.message,
+
+          discoveryResult: discoveryResult,
+          payload: decodingResult.payload,
+          header: decodingResult.header,
+        };
+      })
+      .catch((error) => {
+        return {
+          showResults: false,
+          message: error,
+
+          discoveryResult: '',
+          payload: '',
+          header: '',
+        };
+      });
     return result;
   }
 }
